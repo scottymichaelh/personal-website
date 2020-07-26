f24251194e2ba9f9514fb824aef22b3f497f25cc
scottymichaelh <scott.halasy@gmail.com>
2020-07-26 12:06:11 -0500

    Commit entire directory to ready deployment to Bluehost server

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Scott Halasy</li>
-              <li>Original template by Tim Baker</li>
+              <li>Original template by <a title="Tim Baker" href="https://github.com/tbakerx">Tim Baker</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
